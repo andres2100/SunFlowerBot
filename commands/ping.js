f023c1e2fa33886235c19681fbec8b8c57f31acc
andres2100 <rickygutiaular@gmail.com>
2020-03-31 12:40:32 -0400

    Changes to be able to win

--- commands/ping.js
+++ commands/ping.js
@@ -1,9 +1,19 @@
+/* eslint-disable no-trailing-spaces */
 module.exports = {
 	name: 'ping',
 	description: 'Ping!',
 	cooldown: 5,
 	guildOnly: true,
 	execute(message) {
-		message.channel.send('Pong.');
+		int didwin = Math.random(1,100);
+		if(didwin > 10)
+		{
+			message.channel.send('Pong.');
+		}
+		else
+		{
+			message.channel.send('Ping. WAIT NO!. ok fine you win >:(');
+		}
+		
 	},
 };
